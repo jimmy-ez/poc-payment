2b0723de5a7fa8cb38a1dd663acd2eb3de0bdb85
jimmy-ez <s.pramuansin@gmail.com>
2023-11-06 13:44:21 +0700

    ✨ feat: get native transaction

--- hooks/event.ts
+++ hooks/event.ts
@@ -1,5 +1,8 @@
 import { _abi } from "../typechain/abi/ERC20";
-import { ethers, ContractEventPayload, EventLog } from "ethers"
+import { ethers, ContractEventPayload, EventLog, EventFilter } from "ethers"
+import Moralis from "moralis";
+import { EvmChain } from "@moralisweb3/common-evm-utils";
+import { useState } from "react";
 
 const provider = new ethers.JsonRpcProvider(
     "https://eth-goerli.g.alchemy.com/v2/0VW8Cn6CPGpo5RIWLwyiDIA344e2ulkk",
@@ -17,8 +20,8 @@ export async function getEventsOn(merchantAddress: string, amount: Number): Prom
             const log: EventLog = txEvent.log;
             const [from, to, am] = log.args;
             const recieveAmount = Number(am / BigInt(10 ** 18))
-            console.log(`from: ${from}, to: ${to}, amount: ${am}`);
-            console.log(`recieveAmount: ${recieveAmount}`);
+            // console.log(`from: ${from}, to: ${to}, amount: ${am}`);
+            // console.log(`recieveAmount: ${recieveAmount}`);
             if (to == merchantAddress && amount == recieveAmount) {
                 resolve(txEvent.log);
             }
@@ -30,4 +33,34 @@ export async function getEventsOff() {
     token.removeAllListeners("Transfer")
 }
 
+
+export async function getHistoryEvent(merchantAddress: string, fromBlock: number, toBlock: number) {
+    const filter = token.filters.Transfer(null, merchantAddress);
+    // await token.queryFilter(filter, -10) // from last 10 block
+    await token.queryFilter(filter, fromBlock, toBlock).then((res) => {
+        console.log("res ::: ", res);
+    })
+}
+export async function startMoralis() {
+    await Moralis.start({
+        apiKey: process.env.NEXT_PUBLIC_MORALIS_API_KEY,
+    });
+}
+
+export async function getNativeTransaction(fromBlock: number, toBlock: number) {
+    const address = signer.address;
+    const chain = EvmChain.GOERLI;
+    const response = await Moralis.EvmApi.transaction.getWalletTransactions({
+        address,
+        chain,
+        fromBlock,
+        toBlock
+    });
+    console.log(response.toJSON());
+}
+
+
+// QR code from backend
+// Tricker transaction status
+// fetch previod event from blockNumber
 // re-connect
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { EventLog } from "ethers"
 import { Inter } from 'next/font/google'
-import { getEventsOn, getEventsOff } from '../../hooks/event'
+import { getEventsOn, getEventsOff, getHistoryEvent, getNativeTransaction, startMoralis } from '../../hooks/event'
 import { useState } from 'react'
 import QRCode from "react-qr-code";
 import { Toaster, toast } from 'sonner'
@@ -10,8 +10,11 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   const [address, setAddress] = useState("0x5f278CcD052b2118cE65589820D715a6717d31a4")
   const [amount, setAmount] = useState(0)
+  const [startBlock, setStartBlock] = useState(0)
+  const [endBlock, setEndBlock] = useState(0)
   const [qrValue, setQrValue] = useState("")
   const [isGenerated, setIsGenerated] = useState(false)
+  const [isStartMoralis, setIsStartMoralis] = useState(false)
 
   const handleOn = () => {
     // getEventsOn(address ? address : "")
@@ -33,6 +36,14 @@ export default function Home() {
       getEventsOff()
       setIsGenerated(false)
     })
+  }
+
+  const handleGetNativeTx = async () => {
+    if (!isStartMoralis) {
+      await startMoralis()
+      setIsStartMoralis(true)
+    }
+    getNativeTransaction(startBlock, endBlock)
   }
 
   return (
@@ -60,7 +71,29 @@ export default function Home() {
         <button className='px-4 py-2 bg-purple-400 rounded-md' onClick={() => {
           handleCreateQr()
         }}>Generate QR</button>
+
+        <div className='flex flex-col my-6 justify-center gap-4 text-black'>
+          <input
+            type="number"
+            value={startBlock}
+            onChange={(e) => { setStartBlock(Number(e.target.value)) }}
+            className='w-[300px] h-[40px] rounded-md pl-4'
+            placeholder='Start Block :' />
+          <input
+            type="number"
+            value={endBlock}
+            onChange={(e) => { setEndBlock(Number(e.target.value)) }}
+            className='w-[300px] h-[40px] rounded-md pl-4'
+            placeholder='End Block :' />
+        </div>
+        <button className='mt-4 px-4 py-2 bg-pink-400 rounded-md' onClick={() => {
+          getHistoryEvent(address, startBlock ? startBlock : 9967510, endBlock ? endBlock : 9967583)
+        }}>History Events</button>
       </div>
+
+      <button className='px-4 py-2 bg-orange-400 rounded-md' onClick={() => {
+        handleGetNativeTx()
+      }}>Native Transaction</button>
 
       <div>
         {isGenerated ? <QRCode value={qrValue}></QRCode> : <></>}
